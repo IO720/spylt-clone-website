d83723567107445c10e0b406a327576f201c771e
IO720 <IO_720@protonmail.com>
2025-07-25 17:23:06 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import NavBar from "./components/navbar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavourSection";
+import NutritionSection from "./sections/NutritionSection";
+import { useGSAP } from "@gsap/react";
+import BenefitSection from "./sections/BenefitSection";
+import TestimonialSection from "./sections/TestimonialSection";
+import FooterSection from "./sections/FooterSection";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+  return (
+    <main>
+      <NavBar />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <NutritionSection />
+
+          <div>
+            <BenefitSection />
+            <TestimonialSection />
+          </div>
+          <FooterSection/>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
